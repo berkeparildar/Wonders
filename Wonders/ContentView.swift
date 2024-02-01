64460ae049812cb4bfb55dcbacefee8519b4b3fd
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2024-02-01 23:14:20 +0300

    Initial commit

---
+++ Wonders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wonders
+//
+//  Created by Berke Parıldar on 1.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wonders/WondersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WondersApp.swift
+//  Wonders
+//
+//  Created by Berke Parıldar on 1.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WondersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
